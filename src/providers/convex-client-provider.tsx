06a50a4acf4ba59023b449b1abb8cedb6b665c5b
Manikhandan Mavureddy Senthilvelu <msmanikhandan333@gmail.com>
2025-01-16 00:34:18 -0500

    Authentication and webhooks

--- src/providers/convex-client-provider.tsx
+++ src/providers/convex-client-provider.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import {ClerkProvider, useAuth} from "@clerk/nextjs";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    return (
+        <ClerkProvider publishableKey="pk_test_ZXF1YWwtYmx1ZWpheS03NC5jbGVyay5hY2NvdW50cy5kZXYk">
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
 }
