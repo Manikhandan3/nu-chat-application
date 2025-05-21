fea7fa24cdafface46dba75fe47f4cd670b8efc1
Manikhandan Mavureddy Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2025-05-21 18:16:13 -0400

    updated webhooks - clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://heroic-wasp-17.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
